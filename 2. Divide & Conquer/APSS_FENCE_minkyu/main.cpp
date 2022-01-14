e4bfbc58774580efe0637958fd8aac94e7493882
minkyu <whizkyu@snu.ac.kr>
2022-01-14 17:03:58 +0900

    코드 간략화

--- 2. Divide & Conquer/APSS_FENCE_minkyu/main.cpp
+++ 2. Divide & Conquer/APSS_FENCE_minkyu/main.cpp
@@ -6,76 +6,43 @@
 using namespace std;
 
 int C, N;
+vector<int> fence(20001);
 
-int max(int ret1, int ret2, int ret3) {
-    if (ret1 > ret2 && ret1 > ret3) return ret1;
-    return ret2 > ret3 ? ret2 : ret3;
-}
-
-int getMax(vector<int>& fence, int left, int right) {
+int getMax(int left, int right) {
     // base case 1: left = right 일 경우
     if (left == right) return fence[left];
-    // base case 2: left + 1 = right 일 경우;
-    if (left + 1 == right) {
-        int ret1 = fence[left];
-        int ret2 = fence[right];
-        int ret3 = 2 * (fence[left] < fence[right] ? fence[left] : fence[right]);
-        return max(ret1, ret2, ret3);
-    }
 
     int mid = (left + right) / 2;
-    // 왼쪽 절반 울타리의 최대 넓이
-    int ret1 = getMax(fence, left, mid);
-    // 오른쪽 절반 울타리의 최대 넓이
-    int ret2 = getMax(fence, mid+1, right);
+    // 가운데를 걸치지 않는 울타리의 최대 넓이
+    int ret1 = max(getMax(left, mid), getMax(mid+1, right));
 
     // 아래는 왼쪽과 오른쪽에 둘 다에 걸치는 최대 넓이
     int l = mid, r = mid+1;
     int height = fence[l] < fence[r] ? fence[l] : fence[r];
     int width = 2;
-    int ret3 = width * height;
-    l--, r++;
+    int ret2 = width * height;
     // 중간에 걸친 직사각형의 왼쪽 끝 울타리와 오른쪽 끝 울타리가 각각 l, r이면
     // 울타리 범위 안에서 l, r을 1씩 키워가면서 최대값 찾기
-    while (true) {
-        if (l > left - 1 && r < right + 1) {
-            if (fence[l] < fence[r]) {
-                height = fence[r] < height ? fence[r] : height;
-                int temp = ++width * height;
-                ret3 = temp > ret3 ? temp : ret3;
-                r++;
-            } else {
-                height = fence[l] < height ? fence[l] : height;
-                int temp = ++width * height;
-                ret3 = temp > ret3 ? temp : ret3;
-                l--;
-            }
-        } 
-        else if (r < right + 1) {
-            height = fence[r] < height ? fence[r] : height;
-            int temp = ++width * height;
-            ret3 = temp > ret3 ? temp : ret3;
+    while (left < l || r < right) {
+        if (r < right && (l < left || fence[l-1] < fence[r+1])) {
             r++;
-        } 
-        else if (l > left - 1) {
-            height = fence[l] < height ? fence[l] : height;
-            int temp = ++width * height;
-            ret3 = temp > ret3 ? temp : ret3;
+            height = min(fence[r], height);
+        } else {
             l--;
-        } 
-        else break;
+            height = min(fence[l], height);
+        }
+        ret2 = max(ret2, ++width * height);
     }
-    return max(ret1, ret2, ret3);
+    return max(ret1, ret2);
 }
 
 int main() {
     cin >> C;
     for (int i = 0; i < C; i++) {
         cin >> N;
-        vector<int> fence(N);
         for (int j = 0; j < N; j++) {
             cin >> fence[j];
         }
-        cout << getMax(fence, 0, N-1) << endl;
+        cout << getMax(0, N-1) << endl;
     }
 }
