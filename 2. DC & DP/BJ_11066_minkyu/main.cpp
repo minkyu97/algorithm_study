e5f32050b43822f128e32c55dc72994006a89e7c
minkyu <whizkyu@snu.ac.kr>
2022-02-02 00:30:56 +0900

    11066번 경계 조건 개선

--- 2. DC & DP/BJ_11066_minkyu/main.cpp
+++ 2. DC & DP/BJ_11066_minkyu/main.cpp
@@ -10,6 +10,7 @@ using matrix = vector<vector<int> >;
 int T, K;
 // i번째부터 i+j번째 챕터를 합칠 수 있는 최소 비용 저장
 matrix cache;
+matrix bound;
 vector<int> sum;
 
 // O(n)으로 줄일 수 있음
@@ -23,25 +24,33 @@ int main() {
     while (T--) {
         cin >> K;
         cache = matrix(K+1, vector<int>(K+1, MAX));
+        bound = matrix(K+1, vector<int>(K+1));
         sum = vector<int>(K+1);
         for (int j = 1; j <= K; j++) {
             int a;
             cin >> a;
-            cache[j][0] = 0;
+            cache[j][j] = 0;
+            bound[j][j] = j;
             sum[j] = sum[j-1] + a;
         }
-        for (int diff = 1; diff < K; diff++) {
+        for (int j = 2; j <= K; j++) {
+            cache[j-1][j] = sum[j] - sum[j-2];
+            bound[j-1][j] = j-1;
+        }
+        for (int diff = 2; diff < K; diff++) {
             for (int start = 1; start <= K - diff; start++) {
-                for (int i = 1; i <= diff; i++) {
-                    cache[start][diff] = min(
-                        cache[start][diff],
-                        cache[start][i-1] + cache[start+i][diff-i]
-                    );
+                int end = start+diff;
+                for (int i = bound[start][end-1]; i <= bound[start+1][end]; i++) {
+                    int temp = cache[start][i] + cache[i+1][end];
+                    if (temp < cache[start][end]) {
+                        cache[start][end] = temp;
+                        bound[start][end] = i;
+                    }
                 }
-                cache[start][diff] += calcsum(start, start+diff);
+                cache[start][end] += calcsum(start, end);
             }
         }
         
-        cout << cache[1][K-1] << endl;
+        cout << cache[1][K] << endl;
     }
 }
