8aa5b60557c380c815377a400626a8d566dee2cf
minkyu <whizkyu@snu.ac.kr>
2022-02-01 20:38:59 +0900

    20667번 풀이(비용대신 메모리값을 캐시, 슬라이딩윈도)

--- 2. DC & DP/BJ_20667_minkyu/main.cpp
+++ 2. DC & DP/BJ_20667_minkyu/main.cpp
@@ -3,71 +3,56 @@
 #include <cmath>
 #define endl '\n'
 #define MAX 987654321
+#define MIN -987654321
 
 using namespace std;
 using matrix = vector<vector<int> >;
 using mat3d = vector<matrix>;
 
 int N, M, K;
-int tab[100][3];
-int totalcpu, totalmem, totalpri;
-vector<int> closedTab;
+int cpu[100], mem[100], prt[100];
+int totalcpu = 0, totalmem = 0, totalpri = 0;
 matrix cache;
 
 int cpuindex(int cpu) {
-    if (cpu > M) return M+1;
-    return cpu;
-}
-
-// start이전의 탭들은 어떤 걸 끄고 켤지 이전 재귀에서 결정이 되었다.
-// 그 동안 사용한 중요도와 얻은 cpu, memory를 인자로 받았다.
-// cache에 기록된 것보다 현재 상황이 나아졌다면, 계속 나아간다.
-// 아니라면, 잘못된 선택이므로 MAX를 반환한다.
-void solve(int start, int cpu, int memory, int priority) {
-    if (start == N) return;
-    if (cpu >= M && memory >= K) {
-        cache[start][0] = cpu;
-        cache[start][1] = memory;
-        cache[start][2] = priority;
-        return;
-    }
-    int& cachecpu = cache[start][0];
-    int& cachemem = cache[start][1];
-    int& cachepri = cache[start][2];
-    if (cachecpu >= min(cpu, M) && cachemem >= min(cpu, K) && priority >= cachepri) return;
-    if (cpu >= min(cachecpu, M) && memory >= min(cachemem, K) && priority <= cachepri) {
-        cachecpu = cpu;
-        cachemem = memory;
-        cachepri = priority;
-    }
-    solve(start+1, cpu+tab[start][0], memory+tab[start][1], priority+tab[start][2]);
-    solve(start+1, cpu, memory, priority);
+    if(cpu < M) return cpu;
+    else return M;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin >> N >> M >> K;
-    cache = matrix(N, vector<int>(3, MAX));
+    cache = matrix(M+1, vector<int>(500, MIN));
     for (int i = 0; i < N; i++) {
-        cin >> tab[i][0] >> tab[i][1] >> tab[i][2];
-        totalcpu += tab[i][0];
-        totalmem += tab[i][1];
-        totalpri += tab[i][2];
+        cin >> cpu[i] >> mem[i] >> prt[i];
+        totalcpu += cpu[i];
+        totalmem += mem[i];
+        totalpri += prt[i];
     }
     if (totalcpu < M || totalmem < K) {
         cout << -1 << endl;
         return 0;
     }
     if (totalcpu == M || totalmem == K) {
-        return totalpri;
+        cout << totalpri << endl;
+        return 0;
     }
 
-    solve(0, 0, 0, 0);
-
-    int minpri = MAX;
-    for (int i = 0; i < N; i++) {
-        if (cache[i][0] >= M && cache[i][1] >= K && cache[i][2] < minpri) minpri = cache[i][2];
+    cache[cpu[0]][prt[0]] = mem[0];
+    cache[0][0] = 0;
+    for (int i = 1; i < N; i++) {
+        for (int j = M; j != -1; j--) {
+            for (int k = totalpri; k != -1; k--) {
+                if (cache[j][k] == MIN) continue;
+                cache[cpuindex(j+cpu[i])][k+prt[i]] = max(cache[cpuindex(j+cpu[i])][k+prt[i]], cache[j][k] + mem[i]);
+            }
+        }
     }
 
-    cout << minpri << endl;
+    for (int i = 0; i <= 500; i++) {
+        if (cache[M][i] >= K) {
+            cout << i << endl;
+            return 0;
+        }
+    }
 }
