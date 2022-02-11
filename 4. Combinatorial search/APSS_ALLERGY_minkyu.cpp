8074492e975ad15952e97e8ef83bfb2a98a32006
minkyu <whizkyu@snu.ac.kr>
2022-02-11 15:30:31 +0900

    종만북 알러지 초기 구현

---
+++ 4. Combinatorial search/APSS_ALLERGY_minkyu.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+using matrix = vector<vector<int> >;
+int T, n, m;
+int MAX_DEPTH;
+// nameIndex[name] : table에서 해당 이름을 가진 사람의 행 번호 저장
+map<string, int> nameIndex;
+// table[i] : i번째 음식을 먹을 수 있는 사람들의 리스트
+matrix foodEater;
+// 현재 상태
+vector<int> edible;
+
+void select(int food) {
+    for (int saram: foodEater[food]) {
+        edible[saram]++;
+    }
+}
+
+void unselect(int food) {
+    for (int saram: foodEater[food]) {
+        edible[saram]--;
+    }
+}
+
+// start 번 음식부터 시작
+void solve(int start, int depth) {
+    if (depth >= MAX_DEPTH) return;
+    if (find(edible.begin(), edible.end(), 0) == edible.end()) {
+        MAX_DEPTH = depth;
+        return;
+    }
+    PASS:
+    for (int i = start; i < m; i++) {
+        select(i);
+        solve(i+1, depth+1);
+        unselect(i);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cin >> T;
+    while(T--) {
+        MAX_DEPTH = 21;
+        cin >> n >> m;
+        foodEater = matrix(m);
+        edible = vector<int>(n, 0);
+        for (int i = 0; i < n; i++) {
+            string name;
+            cin >> name;
+            nameIndex[name] = i;
+        }
+        for (int i = 0; i < m; i++) {
+            int num;
+            cin >> num;
+            while(num--) {
+                string name;
+                cin >> name;
+                foodEater[i].push_back(nameIndex[name]);
+            }
+        }
+        solve(0, 0);
+        cout << MAX_DEPTH << endl;
+    }
+}
